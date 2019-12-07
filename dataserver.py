b4464db698f0b1162947c8b1397f47241b5dc3d3
dragonvanken <372936540@qq.com>
2019-12-07 16:37:19 +0800

    add chunk

--- dataserver.py
+++ dataserver.py
@@ -1,18 +1,18 @@
 import grpc
 from protocol import MasterForData_pb2
 from protocol import MasterForData_pb2_grpc
+from datalib import StoreManager
 
-DID = []
 
 def register():
     channel = grpc.insecure_channel('localhost:50051')
     stub = MasterForData_pb2_grpc.MFDStub(channel)
     response = stub.RegisteServer(MasterForData_pb2.socket(ip = 'localhost',port = 50000))
-    DID.append(response.id)
+    StoreManager.StoreManager.setDID(response.id)
 
 def serve():
     register()
 
 if __name__ == '__main__':
     serve()
-    print(DID)
+    print(StoreManager.StoreManager.getDID())
