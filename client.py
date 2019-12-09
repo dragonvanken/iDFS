2826b4539fdddd5101412907166ccd2cbd581ad2
nxy <nxy1104155190@gmail.com>
2019-12-09 20:10:59 +0800

    增加同名检测，更新用户界面

--- client.py
+++ client.py
@@ -66,6 +66,9 @@ def getTree(stub):
 
     filetree.FileTree.deseriesFromPath(newtree)
 
+def isUniquePath(destination):
+    return filetree.FileTree.seek(destination) is None
+
 def upload(stub):
     print('uploading')
     """ try:
@@ -73,8 +76,13 @@ def upload(stub):
     except:
         print(colored('Bad connection.', 'red'))
         print(colored('Please retry.', 'red')) """
-    upfile(stub, 'ppp', 'root/ppp')
-    print('Successfully upload file', 'ppp')
+    path = 'ppp'
+    destination = 'root/ppp'
+    if not isUniquePath(destination):
+        print(colored('Unavailable path, please retry.', 'red'))
+    else:
+        upfile(stub, path, destination)
+        print(colored('Successfully upload file '+path+' to '+destination,'green'))
     fetch(stub)
 
 def fetch(stub):
@@ -87,6 +95,20 @@ def fetch(stub):
         print(colored('Bad connection.', 'red'))
         print(colored('Please retry.', 'red'))
 
+def showAllCommands():
+    print('iDFS is a Distribution File System written by WanKeJia group for NUDT distribution system cource.\n')
+    print('All commands:')
+    print('\tfetch:')
+    print('\t\t更新并显示文件目录结构')
+
+    print('\tupload')
+    print('\t\t上传文件')
+
+    print('\tdelete')
+    print('\t\t删除文件')
+
+    print('\tquit or exit')
+    print('\t\t退出系统')
 # 用户端命令行界面
 def user_interface():
     stub = ConnectMaster()
@@ -94,7 +116,9 @@ def user_interface():
     while(True):
         print(colored("\nPlease input command:", 'green'), end='')
         cmd = input().lower().strip()
-        if cmd == 'fetch':
+        if cmd == 'h' or cmd == 'help':
+            showAllCommands()
+        elif cmd == 'fetch':
             fetch(stub)
         elif cmd == 'upload':
             upload(stub)
